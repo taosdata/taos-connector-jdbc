8205641a16779cce1240fbe6b4ebf905d9fb9de1
Bo Ding <dingbo8128@163.com>
2022-06-07 13:59:14 +0800

    feat: connect to cloud use url and token (#12)
    * feat: connect to cloud use url and token
    
    * enh: comment
    
    * enh: remove sys.out

---
+++ src/test/java/com/taosdata/jdbc/cloud/CloudTest.java
@@ -0,0 +1,43 @@
+package com.taosdata.jdbc.cloud;
+
+
+import com.taosdata.jdbc.rs.RestfulConnection;
+import org.junit.Test;
+
+import java.sql.*;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class CloudTest {
+
+    @Test
+    public void connectCloudService() throws Exception {
+        String url = System.getenv("TDENGINE_CLOUD_URL");
+        String token = System.getenv("TDENGINE_CLOUD_TOKEN");
+        if (url == null || token == null) {
+            System.out.println("Environment variable for CloudTest not set properly");
+            return;
+        }
+        Connection conn = new RestfulConnection(url, token);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select server_version()");
+        rs.next();
+        String version = rs.getString(1);
+        System.out.println(version);
+        assertNotEquals(version, null);
+        stmt.execute("create database if not exists cloudtest");
+        stmt.execute("create table if not exists cloudtest.t0(ts timestamp, c0 binary(20))");
+        stmt.execute("insert into cloudtest.t0 values(now, 'abc')(now+1s, '涛思数据')");
+        rs = stmt.executeQuery("select * from cloudtest.t0");
+        ResultSetMetaData meta = rs.getMetaData();
+        for (int i = 1; i <= meta.getColumnCount(); i++) {
+            System.out.println(meta.getColumnLabel(i) + " " + meta.getColumnTypeName(i));
+        }
+        while (rs.next()) {
+            Timestamp v1 = rs.getTimestamp(1);
+            String v2 = rs.getString(2);
+            System.out.println(v1 + " " + v2);
+        }
+        stmt.execute("drop database if exists cloudtest");
+    }
+}
