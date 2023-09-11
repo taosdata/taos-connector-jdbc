78e436771b96a3ff1abe47d546060c2fc4f944a7
huolibo <huolibo@qq.com>
2023-09-11 16:47:08 +0800

    fix:uint64 test

--- src/main/java/com/taosdata/jdbc/ws/entity/FetchReq.java
+++ src/main/java/com/taosdata/jdbc/ws/entity/FetchReq.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.annotation.JSONField;
 import com.taosdata.jdbc.utils.UInt64Codec;
 
 public class FetchReq extends Payload {
-    @JSONField(serializeUsing = UInt64Codec.class)
+    @JSONField(serializeUsing = UInt64Codec.class, deserializeUsing = UInt64Codec.class)
     private long id;
 
     public long getId() {
